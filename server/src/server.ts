bdd8a1eaada9fbe8a960ff83f7aa77f3473c57d6
Ana Freitas <ana.freitaas16@gmail.com>
2023-05-16 22:56:37 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', () => {
+    return "Hello World!"
+});
+
+app.listen({
+    port: 6006,
+}).then(() => {
+    console.log("Server is running on port 6006");
+});
